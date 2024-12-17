1d9992d15772f02c52b5028972501d985349a32b
Gheorghe <madangheorghe.30@gmail.com>
2024-12-17 13:17:52 +0100

    create com

---
+++ script.js
@@ -0,0 +1,11 @@
+// Biglietto del treno
+// INPUT dei km che il cliente vuole percorrere e l'età 
+// l'utente inserisce il numero di km che percorre
+// il PC  calcola quanto spenderà in base ai km che l'utente farà 
+// l'utente inserisce la sua età
+// SE ha meno di 18 anni 
+    // allora avrà uno sconto del 20%
+// ALTRIMENTI SE ha più di 65 anni 
+    // allora avrà uno sconto del 40%
+// ALTRIMENTI paga il prezzo intero
+// IL PC CI DA IL RISULTATO CON DUE DECIMALI
